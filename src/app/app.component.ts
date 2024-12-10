82187494fc2ae93ec9f1c2157664dd65dff1f472
Mykola Diachok <ndyachok@gmail.com>
2024-12-10 05:04:12 +0100

    add assets and meta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LandingPageComponent} from './components/landing-page/landing-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandingPageComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'it-systems';
+  title = 'Development of IT systems - Croatia | .NET, C#, SQL, Angular, Azure experts';
 }
---
+++ src/app/components/landing-page/landing-page.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {Meta, Title} from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [],
+  templateUrl: './landing-page.component.html',
+  standalone: true,
+  styleUrl: './landing-page.component.css'
+})
+export class LandingPageComponent implements OnInit {
+  constructor(private meta: Meta, private title: Title) {}
+
+  ngOnInit(): void {
+    this.title.setTitle('Development of IT Systems - Croatia | .NET, C#, SQL, Angular, Azure Experts');
+
+
+    this.meta.addTag({
+      name: 'description',
+      content: 'Expert IT development services specializing in .NET, C#, SQL, Angular, Azure, and enterprise solutions. Delivering top-notch software engineering and cloud services.'
+    });
+
+
+    this.meta.addTag({
+      name: 'keywords',
+      content: 'IT development, .NET, C#, SQL, Angular, Azure, web application development, WAS, GitHub, software engineering, cloud solutions, frontend, backend, DevOps, APIs, REST, system architecture, database design, enterprise solutions'
+    });
+
+    this.meta.addTag({
+      name: 'author',
+      content: 'Mykola Diachok'
+    });
+  }
+}
